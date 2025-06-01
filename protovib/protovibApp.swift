c34aeed932634e8567da9f82003dd4e51e80651c
Park Junyoung <bloomwayz@snu.ac.kr>
2025-06-02 04:35:31 +0900

    🎉 Initial Commit

---
+++ protovib/protovibApp.swift
@@ -0,0 +1,17 @@
+//
+//  protovibApp.swift
+//  protovib
+//
+//  Created by young on 28/5/25.
+//
+
+import SwiftUI
+
+@main
+struct protovibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ protovibTests/protovibTests.swift
@@ -0,0 +1,17 @@
+//
+//  protovibTests.swift
+//  protovibTests
+//
+//  Created by young on 5/28/25.
+//
+
+import Testing
+@testable import protovib
+
+struct protovibTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
