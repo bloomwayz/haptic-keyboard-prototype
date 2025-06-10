afc143d89f572291367ea58e31974716917286c2
Park Junyoung <bloomwayz@snu.ac.kr>
2025-06-11 00:11:06 +0900

    :truck: `ContentView.swift` → `protovib/ContentView.swift`

--- protovib/ContentView.swift
+++ protovib/ContentView.swift
@@ -6,8 +6,6 @@ struct ContentView: View {
     @State private var currentBlock: (row: Int, col: Int)? = nil
     @State private var longPressTimer: Timer?
     @State private var longPressTriggered = false
-    
-    // 키보드 구현을 위한 state 선언
     @State private var inputText: String = ""
     @State private var isShifted: Bool = false // Shift state
 
@@ -15,8 +13,8 @@ struct ContentView: View {
     let cols = 5
 
     let blockLabels: [String] = [
-        "Q", "W", "⌦", "O", "P",
-        "A", "S", "⌦", "K", "L",
+        "Q", "W", "⌫", "O", "P",
+        "A", "S", "⌫", "K", "L",
         "Z", "X", "⇧", "N", "M",
         "E", "R", "␣", "U", "I",
         "D", "F", "␣", "H", "J",
@@ -24,6 +22,7 @@ struct ContentView: View {
         "T", "G", "⏎", ",", "?"
     ]
 
+    // keyboard layout
     var body: some View {
         GeometryReader { geo in
             VStack {
@@ -51,6 +50,8 @@ struct ContentView: View {
                 }
                 .gesture(
                     DragGesture(minimumDistance: 0)
+                    
+                        // on key press: play block haptic
                         .onChanged { value in
                             let block = self.blockAt(location: value.location, in: geo.size)
                             if block?.row != self.currentBlock?.row || block?.col != self.currentBlock?.col {
@@ -60,21 +61,17 @@ struct ContentView: View {
                                 if let block = block {
                                     self.prepareHaptics()
                                     self.playBlockHaptic(row: block.row, col: block.col)
-                                    //self.longPressTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
-                                    //self.longPressTriggered = true
-                                    //self.prepareHaptics()
-                                    //self.playBlockHaptic(row: block.row, col: block.col)
-                                    //}
                                 }
                             }
                         }
+                    
+                        // on key up: play success haptic
                         .onEnded { _ in
                             self.longPressTimer?.invalidate()
                             if let block = self.currentBlock {
                                 let feedback = UINotificationFeedbackGenerator()
                                 feedback.prepare()
                                 feedback.notificationOccurred(.success)
-                                self.handleKeyInput(row: block.row, col: block.col)
                             }
                             self.currentBlock = nil
                         }
@@ -98,7 +95,7 @@ struct ContentView: View {
             switch label {
             case "␣":
                 return .yellow.opacity(0.7)
-            case "⌦":
+            case "⌫":
                 return .red.opacity(0.7)
             case "⇧":
                 return .blue.opacity(0.7)
@@ -121,54 +118,54 @@ struct ContentView: View {
         }
     }
 
-    private func playHaptics(events: [CHHapticEvent]) {
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play haptic: \(error.localizedDescription)")
-        }
-    }
-
     func playBlockHaptic(row: Int, col: Int) {
         switch (row, col) {
-        case (0,0): HapticManager.doHaptics_test_Q(engine: engine) // Q
-        case (0,1): HapticManager.doHaptics_test_W(engine: engine) // W
-        case (0,2), (1,2): doHaptics_delete() // backspace
+        //case (0,0): HapticManager.doHaptics_00(engine: engine) // Q
+        case (0,0): HapticManager.doHaptics_Q(engine: engine) // Q
+        //case (0,1): HapticManager.doHaptics_01(engine: engine) // W
+        case (0,1): HapticManager.doHaptics_W(engine: engine) // W
+        case (0,2): doHaptics_delete() // Backspace
         case (0,3): HapticManager.doHaptics_03(engine: engine) // O
         case (0,4): HapticManager.doHaptics_04(engine: engine) // P
 
-        case (1,0): HapticManager.doHaptics_test_A(engine: engine) // A
-        case (1,1): HapticManager.doHaptics_test_S(engine: engine) // S
+        //case (1,0): HapticManager.doHaptics_10(engine: engine) // A
+        case (1,0): HapticManager.doHaptics_A(engine: engine) // A
+        //case (1,1): HapticManager.doHaptics_11(engine: engine) // S
+        case (1,1): HapticManager.doHaptics_S(engine: engine) // S
+        //case (1,2): HapticManager.doHaptics_12(engine: engine) // Backspace
+        //case (1,2): HapticManager.doHaptics_02(engine: engine) // Backspace
         case (1,3): HapticManager.doHaptics_13(engine: engine) // K
         case (1,4): HapticManager.doHaptics_14(engine: engine) // L
 
-        case (2,0): HapticManager.doHaptics_test_Z(engine: engine) // Z
-        case (2,1): HapticManager.doHaptics_test_X(engine: engine) // X
+        case (2,0): HapticManager.doHaptics_20(engine: engine) // Z
+        case (2,1): HapticManager.doHaptics_21(engine: engine) // X
         case (2,2): doHaptics_shift() // shift
         case (2,3): HapticManager.doHaptics_23(engine: engine) // N
         case (2,4): HapticManager.doHaptics_24(engine: engine) // M
 
-        case (3,0): HapticManager.doHaptics_test_E(engine: engine) // E
-        case (3,1): HapticManager.doHaptics_test_R(engine: engine) // R
+        case (3,0): HapticManager.doHaptics_30(engine: engine) // E
+        case (3,1): HapticManager.doHaptics_31(engine: engine) // R
         case (3,2), (4,2): doHaptics_space() // space
         case (3,3): HapticManager.doHaptics_33(engine: engine) // U
         case (3,4): HapticManager.doHaptics_34(engine: engine) // I
 
-        case (4,0): HapticManager.doHaptics_test_D(engine: engine) // D
-        case (4,1): HapticManager.doHaptics_test_F(engine: engine) // F
+        case (4,0): HapticManager.doHaptics_40(engine: engine) // D
+        case (4,1): HapticManager.doHaptics_41(engine: engine) // F
+        //case (4,2): HapticManager.doHaptics_42(engine: engine) // space
+        //case (4,2): HapticManager.doHaptics_31(engine: engine) // space
         case (4,3): HapticManager.doHaptics_43(engine: engine) // H
         case (4,4): HapticManager.doHaptics_44(engine: engine) // J
 
-        case (5,0): HapticManager.doHaptics_test_C(engine: engine) // C
-        case (5,1): HapticManager.doHaptics_test_V(engine: engine) // V
+        case (5,0): HapticManager.doHaptics_50(engine: engine) // C
+        case (5,1): HapticManager.doHaptics_51(engine: engine) // V
         case (5,2), (6,2): doHaptics_return() // enter
         case (5,3): HapticManager.doHaptics_53(engine: engine) // B
         case (5,4): HapticManager.doHaptics_54(engine: engine) // Y
 
-        case (6,0): HapticManager.doHaptics_test_T(engine: engine) // T
-        case (6,1): HapticManager.doHaptics_test_G(engine: engine) // G
+        case (6,0): HapticManager.doHaptics_60(engine: engine) // T
+        case (6,1): HapticManager.doHaptics_61(engine: engine) // G
+        //case (6,2): HapticManager.doHaptics_62(engine: engine) // enter
+        //case (6,2): HapticManager.doHaptics_52(engine: engine) // enter
         case (6,3): HapticManager.doHaptics_63(engine: engine) // ,
         case (6,4): HapticManager.doHaptics_64(engine: engine) // ?
 
@@ -181,19 +178,19 @@ struct ContentView: View {
         feedback.prepare()
         feedback.notificationOccurred(.error)
     }
-
+    
     func doHaptics_shift() {
         let feedback = UISelectionFeedbackGenerator()
         feedback.prepare()
         feedback.selectionChanged()
     }
-
+    
     func doHaptics_space() {
         let feedback = UINotificationFeedbackGenerator()
         feedback.prepare()
         feedback.notificationOccurred(.success)
     }
-
+    
     func doHaptics_return() {
         let feedback = UINotificationFeedbackGenerator()
         feedback.prepare()
@@ -211,7 +208,7 @@ struct ContentView: View {
             inputText.append("\n")
         case "⇧":
             isShifted.toggle()
-        case "⌦":
+        case "⌫":
             if !inputText.isEmpty {
                 inputText.removeLast()
             }
